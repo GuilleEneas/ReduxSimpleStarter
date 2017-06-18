ccb8a87698c45a658d07e093b98d02a0175fc48d
Guillermo <guille.eneas@gmail.com>
2017-06-18 18:31:48 +0200

    change app to a function component

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './search_bar';
 
-export default class App extends Component {
-	render() {
-		return (
-			<div>
-				<SearchBar />
-			</div>
-		);
-	}
-}
+// export default class App extends Component {
+// 	render() {
+// 		return (
+// 			<div>
+// 				<SearchBar />
+// 			</div>
+// 		);
+// 	}
+// }
+
+export default () => (
+	<div>
+		<SearchBar />
+	</div>
+);
