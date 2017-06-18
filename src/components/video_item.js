31f19bab85bf341e6dc04ab440464f298a874cdd
Guillermo <guille.eneas@gmail.com>
2017-06-18 19:39:52 +0200

    add key to map

--- src/components/video_item.js
+++ src/components/video_item.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
 export default (props) => {
+	const video = props.video;
 	return <li> Video </li>;
 }
--- src/components/video_list.js
+++ src/components/video_list.js
@@ -3,7 +3,8 @@ import React from 'react';
 import VideoItem from './video_item';
 
 export default (props) => {
-	const videos = props.videos.map((video, index) => <VideoItem key={index} video={video} />);
+	const videos = props.videos.map(video => <VideoItem key={video.etag} video={video} />);
+
 	return (
 		<ul className="col-md-4 list-group">
 			{videos}
